92e8e96d1a629ce0834bc3512f414b79b1303eec
abangrephol <abang.zeze@gmail.com>
2015-02-11 16:55:31 +0700

    Fix appAdmin.js

--- public/apps/appAdmin.js
+++ public/apps/appAdmin.js
@@ -1,11 +1,11 @@
 (function(){
     ifrm = document.createElement("IFRAME");
-    ifrm.setAttribute("src", "http://localhost:2121/loginAPI?apikey="+options.apikey);
+    ifrm.setAttribute("src", "http://appointment.dev6.wirednest.net/loginAPI?apikey="+options.apikey);
     ifrm.setAttribute("width","100%");
     ifrm.setAttribute("frameborder","0");
     document.getElementById(options.element).appendChild(ifrm);
     resizer= document.createElement('script');
-    resizer.setAttribute('src','http://localhost:2121/themes/default/assets/js/iframeResizer.min.js');
+    resizer.setAttribute('src','http://appointment.dev6.wirednest.net/themes/default/assets/js/iframeResizer.min.js');
     document.getElementById(options.element).appendChild(resizer);
     if (resizer.readyState) {
         resizer.onreadystatechange = function() {
