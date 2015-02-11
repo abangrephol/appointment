19bdd7be8f147dfbc57659cecaa47cd418332f47
abangrephol <abang.zeze@gmail.com>
2015-02-11 14:01:40 +0700

    Filters

--- app/filters.php
+++ app/filters.php
@@ -13,7 +13,7 @@
 
 App::before(function($request)
 {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: personalfitnesscoach.sg');
     header('Access-Control-Allow-Methods: *'); //GET, POST, OPTIONS
     header('Access-Control-Allow-Headers: *, apikey '); //Origin, Content-*, Accept, Authorization, X-Request-With
     header('Access-Control-Allow-Credentials: true');
