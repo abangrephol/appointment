ebd8c2ee7225acc73606b024f4b5a68949af7ad2
abangrephol <abang.zeze@gmail.com>
2015-02-12 23:53:31 +0700

    cors

--- app/filters.php
+++ app/filters.php
@@ -13,12 +13,12 @@
 
 App::before(function($request)
 {
-    /*
+
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: *'); //GET, POST, OPTIONS
     header('Access-Control-Allow-Headers: *, apikey '); //Origin, Content-*, Accept, Authorization, X-Request-With
     header('Access-Control-Allow-Credentials: true');
-    */
+
 });
 
 
